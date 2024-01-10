8dadfa2f7dd158763f53145d74e7b2c2884a350e
ASUS <ASUS@LAPTOP-CT0AS4K5>
2024-01-10 05:16:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+[TestClass]
+public class CalculatorTests
+{
+    [TestMethod]
+    public void TestCalculator()
+    {
+        string input = "+\n5\n*\n10\n-\n3\nq\n";
+        string expectedOutput = "[#1]: 0\r\n" +
+                                "[#2]: 5\r\n" +
+                                "[#3]: 50\r\n" +
+                                "[#4]: 47\r\n" +
+                                "Программа завершена.\r\n";
+        var consoleInput = new StringReader(input);
+        var consoleOutput = new StringWriter();
+        Console.SetIn(consoleInput);
+        Console.SetOut(consoleOutput);
+
+        Calculator.Main();
+
+        Assert.AreEqual(expectedOutput, consoleOutput.ToString());
+    }
+}
